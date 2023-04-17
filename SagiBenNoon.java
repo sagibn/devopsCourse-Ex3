d0eb7ed5c19e13cd63dcdcec7ec56a78d3568353
sagibn <sagi838@gmail.com>
2023-04-17 23:13:53 +0300

    update SagiBenNoon.java

--- SagiBenNoon.java
+++ SagiBenNoon.java
@@ -1,7 +1,7 @@
 public class SagiBenNoon{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Sagi Ben Noon");
+			System.out.println("Sagi Ben Noon-change");
 		}
 	}
 }
